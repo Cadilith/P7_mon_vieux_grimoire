b1d079c0468880a19ff31e0922c39b0790e857e1
Tiffanie Orsoni <tiffanie.orsoni@gmail.com>
2023-06-27 09:35:29 +0200

    keep one decimal for average rating

--- models/Book.js
+++ models/Book.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const bookSchema = mongoose.Schema({
     userId: { type: String, required: true },
-    title: { type: String, required: true },
+    title: { type: String, required: true, unique: true },
     author: { type: String, required: true },
     imageUrl: { type: String, required: true },
     year: { type: Number, required: true },
@@ -10,9 +11,9 @@ const bookSchema = mongoose.Schema({
     ratings: [{ userId: String, grade: Number }],
     averageRating: {
         type: Number,
-        //getter to round the average rating
+        //getter to round the average rating (1 decimal)
         get: function (v) {
-            return Math.round(v);
+            return Math.round(v * 10) / 10;
         },
         required: true
     },
