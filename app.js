bde7b80dcc6dde9db7a5a3ae59579e9c9abbf06a
Tiffanie Orsoni <tiffanie.orsoni@gmail.com>
2023-07-03 16:02:52 +0200

    helmet and sanitize added for db security

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 const path = require('path');
+
 
 //routes
 const booksRoutes = require('./routes/books');
@@ -11,6 +14,7 @@ const app = express();
 //CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Cross-Origin-Resource-Policy', 'same-site');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
@@ -26,6 +30,12 @@ mongoose.connect('mongodb+srv://tiffanieorsoni:CgMb2IXKbnH3zjBX@cluster0.r42tbpu
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use(express.json());
+//helmet and sanitize prevent database injections
+app.use(mongoSanitize());
+app.use(helmet({
+    crossOriginEmbedderPolicy: false,
+    crossOriginResourcePolicy: false,
+}));
 app.use('/api/books', booksRoutes);
 app.use('/api/auth', userRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
